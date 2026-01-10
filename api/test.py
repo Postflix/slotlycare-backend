b1dca59a4402c3e06c619dce250a516d0967a400
Postflix <tonon834@gmail.com>
2026-01-10 13:53:41 -0300

    Delete api/test.py

--- api/test.py
+++
@@ -1,6 +0,0 @@
-def handler(request):
-    return {
-        'statusCode': 200,
-        'headers': {'Content-Type': 'application/json'},
-        'body': '{"success": true, "message": "Test function works!"}'
-    }
